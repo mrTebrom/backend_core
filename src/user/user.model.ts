8d5347efe78f3a350e4405bf17dfa46f97bc3e48
mrTebrom <mr.tebron@gmail.com>
2024-06-18 12:28:49 +0500

    user model

---
+++ src/user/user.model.ts
@@ -0,0 +1,69 @@
+import {
+  Table,
+  Column,
+  Model,
+  PrimaryKey,
+  AutoIncrement,
+  DataType,
+} from "sequelize-typescript";
+import { CreateUserDto } from "./user.dto";
+
+@Table({ tableName: "user" })
+export class User extends Model<User, CreateUserDto> {
+  @PrimaryKey
+  @AutoIncrement
+  @Column
+  id: number;
+
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+    validate: {
+      len: [3, 20],
+      notEmpty: true,
+    },
+  })
+  username: string;
+
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+    validate: {
+      isEmail: true,
+      notEmpty: true,
+    },
+  })
+  email: string;
+
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+    validate: {
+      is: /^[0-9]+$/,
+      notEmpty: true,
+    },
+  })
+  phone: string;
+
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+    validate: {
+      len: [6, 20],
+      notEmpty: true,
+    },
+  })
+  password: string;
+
+  @Column({
+    type: DataType.STRING,
+    allowNull: true,
+  })
+  firstName?: string;
+
+  @Column({
+    type: DataType.STRING,
+    allowNull: true,
+  })
+  lastName?: string;
+}
